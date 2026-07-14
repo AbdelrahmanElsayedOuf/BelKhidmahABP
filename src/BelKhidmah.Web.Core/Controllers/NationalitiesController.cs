8de78796da4295e7cce856b37bd64c94a37a9a97
Abd-Elrahman Elsayed <abdoouf303@gmail.com>
2026-07-14 16:23:27 +0300

    Refactor lookup endpoints into dedicated controllers
    Split the monolithic LookupsController into separate, focused controllers for each lookup type (InqueryTypes, Nationalities, Professions). This follows the Single Responsibility Principle and improves maintainability by organizing related endpoints into their own controllers.
    
    Note: CitiesController is not included in this refactoring.

---
+++ src/BelKhidmah.Web.Core/Controllers/NationalitiesController.cs
@@ -0,0 +1,17 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BelKhidmah.Controllers
+{
+    [Route("api/Nationalities")]
+    public class NationalitiesController : BelKhidmahProxyControllerBase
+    {
+        public NationalitiesController(IHttpClientFactory factory, IConfiguration configuration) : base(factory, configuration) { }
+
+        [HttpGet("Get")]
+        public Task<IActionResult> Get()
+            => ProxyAsync(BuildRequest(HttpMethod.Get, "api/Lookups/Nationalities"));
+    }
+}
---
+++ src/BelKhidmah.Web.Core/Controllers/ProfessionsController.cs
@@ -0,0 +1,17 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BelKhidmah.Controllers
+{
+    [Route("api/Professions")]
+    public class ProfessionsController : BelKhidmahProxyControllerBase
+    {
+        public ProfessionsController(IHttpClientFactory factory, IConfiguration configuration) : base(factory, configuration) { }
+
+        [HttpGet("Get")]
+        public Task<IActionResult> Get()
+            => ProxyAsync(BuildRequest(HttpMethod.Get, "api/Lookups/Professions"));
+    }
+}
